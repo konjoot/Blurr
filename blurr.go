40bb1db5ccc06134ce5670f76062691b18d6a719
Sergey Maksimov <sergey.maximov@ncloudtech.ru>
2016-08-11 19:53:38 +0300

    refactoring: remove redundant && legacy code (part 1)

--- blurr.go
+++ blurr.go
@@ -36,42 +36,16 @@
 package skat
 
 import (
-	"database/sql"
-	"engine"
-	"engine/deps"
-	"logger"
 	"os"
 	"os/signal"
-	"regulus/mrs"
-	"skat/client"
-	"skat/config"
-	"skat/hooks"
-	"skat/pool"
-	"skat/redis"
-	"skat/task"
-	"statsd"
 	"time"
 
-	as "logger/helpers"
-	mizar "mizar/client"
-	regulus "regulus/client"
-	rn "skat/redis/notification"
-	vega "vega/client"
-
-	. "engine/deps/helpers"
-	_ "github.com/lib/pq"
 	"golang.org/x/net/context"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
-	. "logger/interfaces"
-	"proto/alcor/groups"
-	"proto/alcor/users"
-	"proto/sirius/messages"
-	. "skat/helpers/context"
 
-	"proto/sirius/attachments"
-	"proto/sirius/rooms"
-	"skat/notification"
+	"github.com/konjoot/blurr/config"
+	"github.com/konjoot/blurr/hooks"
+	"github.com/konjoot/blurr/pool"
+	"github.com/konjoot/blurr/task"
 )
 
 // Конструктор приложения, принимает конфиг
@@ -81,106 +55,9 @@ func New(conf *config.Config) *App {
 		conf = config.New()
 	}
 
-	logger := logger.NewManager()
-	logger.AddTags(`service`, `skat`)
-
-	db, err := sql.Open("postgres", conf.DB)
-	if err != nil {
-		logger.Print(as.Error(err))
-		return nil
-	}
-
-	rm := engine.NewRedis(conf.RedisURL)
-
-	// клиент к мизару
-	mizar, err := mizar.New(conf.MizarURL)
-	if err != nil {
-		logger.Print(as.Error(err))
-		return nil
-	}
-
-	// клиент к веге
-	vega, err := vega.New(conf.VegaURL)
-	if err != nil {
-		logger.Print(as.Error(err))
-		return nil
-	}
-
-	// клиент к регулусу
-	regulus := regulus.New(conf.RegulusURL)
-
-	// клиент межсерверного взаимодействия
-	client := client.New(vega, mizar, regulus, conf.Login, conf.Pass)
-
-	// Конфигурация пула коннектов к БД
-	db.SetMaxOpenConns(conf.DBPoolSize * 3 / 2)
-	db.SetMaxIdleConns(conf.DBPoolSize)
-
-	dbm := mrs.NewDBM(db)
-
-	// регистрируем зависимости для дальнейшего мониторинга доступности
-	deps.Add(Redis(rm.Check))
-	deps.Add(DB(dbm.Check))
-
 	app := &App{
-		DBM:      dbm,
-		RM:       rm,
-		Logger:   logger,
-		Client:   client,
-		Notifier: rn.NewNotifier(),
-
 		Config: conf,
 		done:   make(chan struct{}),
-	}
-
-	if conf.GrpcOn {
-
-		var opts []grpc.DialOption
-
-		if conf.TLSOn {
-			var sn string
-			if conf.TLSHostOverride != "" {
-				sn = conf.TLSHostOverride
-			}
-			var creds credentials.TransportCredentials
-			if conf.CA != "" {
-				var err error
-				creds, err = credentials.NewClientTLSFromFile(conf.CA, sn)
-				if err != nil {
-					app.Logger.Print(as.Error(err))
-				}
-			} else {
-				creds = credentials.NewClientTLSFromCert(nil, sn)
-			}
-			opts = append(opts, grpc.WithTransportCredentials(creds))
-		} else {
-			opts = append(opts, grpc.WithInsecure())
-		}
-
-		siriusConn, err := grpcConn(conf.SiriusAddr, opts...)
-		if err != nil {
-			logger.Print(as.Error(err))
-			return nil
-		}
-
-		siriusMessages := messages.NewMessagesClient(siriusConn)
-		siriusAttachments := attachments.NewAttachmentsClient(siriusConn)
-		siriusRooms := rooms.NewRoomClient(siriusConn)
-
-		alcorConn, err := grpcConn(conf.AlcorAddr, opts...)
-		if err != nil {
-			logger.Print(as.Error(err))
-			return nil
-		}
-
-		alcorGroups := groups.NewGroupsClient(alcorConn)
-		alcorUsers := users.NewUsersClient(alcorConn)
-
-		app.AlcorGroups = alcorGroups
-		app.AlcorUsers = alcorUsers
-		app.SiriusMessages = siriusMessages
-		app.SiriusAttachments = siriusAttachments
-		app.SiriusRooms = siriusRooms
 	}
 
 	return app
@@ -188,57 +65,22 @@ func New(conf *config.Config) *App {
 
 // Основная структура приложения
 type App struct {
-	// пулл коннектов к БД
-	DBM *mrs.DBM
-	// пулл коннектов к редис
-	RM engine.RedisManager
 	// конфиг
 	Config *config.Config
-	// логгер
-	Logger LogManager
-	// клиент межсервисного взаимодействия
-	Client client.Clienter
-
-	// grpc clients
-	AlcorGroups       groups.GroupsClient
-	AlcorUsers        users.UsersClient
-	SiriusMessages    messages.MessagesClient
-	SiriusAttachments attachments.AttachmentsClient
-	SiriusRooms       rooms.RoomClient
-
-	Notifier notification.Notifier
-
 	// семафор завершения приложения
 	done chan struct{}
 }
 
-func grpcConn(addr string, opts ...grpc.DialOption) (conn *grpc.ClientConn, err error) {
-	conn, err = grpc.Dial(addr, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return conn, nil
-}
-
 // Запуск приложения
 func (app *App) Run() {
-	statsd.Init()
 
 	// инициализируем пул воркеров и получаем канал для отправки заданий (jobs) в работу
 	pool, err := pool.New(app.Config.WorkerPoolSize, app.Config.Greedy)
 	if err != nil {
-		app.Logger.Print(as.Error(err))
+		// логируем
 		return
 	}
 	defer pool.Wait()
-
-	// инициализиуем слушатель
-	listener, err := redis.NewListener(app.RM, app.Logger)
-	if err != nil {
-		app.Logger.Print(as.Error(err))
-		return
-	}
-	defer listener.Wait()
 
 	// подписываемся на системные сигналы
 	interrupt := make(chan os.Signal)
@@ -252,8 +94,6 @@ func (app *App) Run() {
 	// основной цикл приложения
 	for keepWorking {
 		select {
-		case <-listener.Out: // process jobs from sender
-			send(app, pool.In)
 		case <-ticker.C: // process jobs from ticker
 			send(app, pool.In)
 		case <-app.done: // shutdown by code
@@ -264,7 +104,6 @@ func (app *App) Run() {
 	}
 
 	// завершаем работу
-	listener.Cancel()
 	pool.Cancel()
 }
 
@@ -276,22 +115,13 @@ func (app *App) Stop() {
 func (app *App) context() context.Context {
 	ctx := context.Background()
 
-	ctx = Client_To(ctx, app.Client)
-	ctx = Redis_To(ctx, app.RM)
-	ctx = AlcorGroups_To(ctx, app.AlcorGroups)
-	ctx = AlcorUsers_To(ctx, app.AlcorUsers)
-	ctx = SiriusMessages_To(ctx, app.SiriusMessages)
-	ctx = SiriusAttachments_To(ctx, app.SiriusAttachments)
-	ctx = SiriusRooms_To(ctx, app.SiriusRooms)
-	ctx = Notifier_To(ctx, app.Notifier)
-
 	return ctx
 }
 
 // Неблокирующая отправка задания в пул
 func send(app *App, in chan task.Performer) {
 	select {
-	case in <- &task.Task{DBM: app.DBM, Logger: app.Logger, Client: app.Client, Redis: app.RM, Ctx: app.context()}:
+	case in <- &task.Task{Ctx: app.context()}:
 		hooks.OnProcess()
 	default:
 	}
--- queue/queue.go
+++ queue/queue.go
@@ -4,65 +4,18 @@ package queue
 
 import (
 	"errors"
-	"regulus/mrs"
 )
 
-// Следующее задание очереди
-const NEXT = `
-		SELECT
-			id,
-			type,
-			meta,
-			count,
-			error
-		FROM queue
-		WHERE lock = false
-		ORDER BY updated_at
-		FOR UPDATE OF queue SKIP LOCKED
-		LIMIT 1`
-
-// Перемещение задания в хвост очереди
-const PUSH = `
-		UPDATE queue
-		SET
-			updated_at = timezone('utc'::text, now()),
-			count = count+1
-		WHERE id = $1`
-
-// Удаляем задание из очереди
-const POP = `DELETE FROM queue WHERE id = $1`
-
-// Блокировка
-const UNLOCK = `UPDATE queue SET lock = false where id = $1`
-
-// Снятие блокировки
-const LOCK = `UPDATE queue SET lock = true where id = $1`
-
-// Получение элемента очереди для обновления
-const SELECT = `
-		SELECT
-			id
-		FROM queue
-		WHERE id = $1
-		FOR UPDATE OF queue SKIP LOCKED
-		LIMIT 1`
-
-// Кол-во задач в очереди
-const COUNT = `
-	SELECT
-		count(1)
-	FROM
-		queue
-	WHERE
-		lock = false`
+var (
+	ErrEmpty = errors.New("queue is empty")
+)
 
 // Конструктор менеджера очереди
-func New(dbh *mrs.DBH) Queuer {
-	return &q{dbh: dbh}
+func New() Queuer {
+	return &q{}
 }
 
 type q struct {
-	dbh *mrs.DBH
 }
 
 type Data struct {
@@ -77,78 +30,20 @@ type Data struct {
 func (q *q) Next() (data *Data, err error) {
 	data = &Data{}
 
-	if _, err = q.dbh.QBegin(); err != nil {
-		return
-	}
-
-	defer func() {
-		q.dbh.CommitOrRollback(err)
-	}()
-
-	var storedErr string
-	// запрашиваем следующую незаблокированную запись из очереди и пытаемся повесить на нее блокировку
-	if err = q.dbh.QueryRow(NEXT).Scan(&data.ID, &data.Type, &data.Meta, &data.Count, &storedErr); err != nil {
-		return
-	}
-
-	if storedErr != "" {
-		data.Err = errors.New(storedErr)
-	}
-
-	if _, err = q.dbh.Exec(LOCK, data.ID); err != nil {
-		return
-	}
-
-	return
+	return data, nil
 }
 
 // Снимает блокировку и перемещаем задание в хвост очереди
 func (q *q) Push(id string) (err error) {
-
-	if _, err = q.dbh.QBegin(); err != nil {
-		return
-	}
-
-	defer func() {
-		q.dbh.CommitOrRollback(err)
-	}()
-
-	if _, err = q.dbh.Exec(SELECT, id); err != nil {
-		return
-	}
-
-	if _, err = q.dbh.Exec(PUSH, id); err != nil {
-		return
-	}
-
-	_, err = q.dbh.Exec(UNLOCK, id)
-
-	return
+	return nil
 }
 
 // Удаляет задание из очереди
 func (q *q) Pop(id string) (err error) {
-	if _, err = q.dbh.QBegin(); err != nil {
-		return
-	}
-
-	defer func() {
-		q.dbh.CommitOrRollback(err)
-	}()
-
-	if _, err = q.dbh.Exec(SELECT, id); err != nil {
-		return
-	}
-
-	_, err = q.dbh.Exec(POP, id)
-
-	return
+	return nil
 }
 
 // кол-во оставшихся заданий очереди
 func (q *q) Count() (count int) {
-
-	q.dbh.QueryRow(COUNT).Scan(&count)
-
 	return
 }
